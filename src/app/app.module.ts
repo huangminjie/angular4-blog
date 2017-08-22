1571432954c305552094222b79ffc4a4b68a7218
黄敏杰 <Aghanim@huangminjiedeiMac.local>
2017-08-22 11:53:53 +0800

    引入material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
