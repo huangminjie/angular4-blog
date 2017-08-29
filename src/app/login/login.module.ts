89cb1fe9b43691458ffc57f6d4b85b02c60a735f
Aghanim42 <756402018@qq.com>
2017-08-29 15:50:38 +0800

    样式调整

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared';
@@ -8,7 +7,6 @@ import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
-    CommonModule,
     SharedModule,
     LoginRoutingModule,
     ReactiveFormsModule
