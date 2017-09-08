a3a5de9d03a6fd2a6ceda250eaf1dc8132e4a340
黄敏杰 <Aghanim@huangminjiedeiMac.local>
2017-09-08 18:16:08 +0800

    新增loader组件和snackbar消息提示服务

--- src/app/core/http.factory.ts
+++ src/app/core/http.factory.ts
@@ -1,6 +1,7 @@
 import { XHRBackend, Http, RequestOptions } from "@angular/http";
 import { InterceptedHttp } from "./http.Interceptor.service";
+import { LoaderService } from './loader/loader.service';
 
-export function HttpFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions): Http {
-    return new InterceptedHttp(xhrBackend, requestOptions);
+export function HttpFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions, loaderService: LoaderService): Http {
+    return new InterceptedHttp(xhrBackend, requestOptions, loaderService);
 }
---
+++ src/app/shared/snackbar.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
+
+@Injectable()
+export class SnackbarService {
+
+    constructor(public snackBar: MdSnackBar) { }
+    autoHide: number = 2000;
+    // 成功
+    public success(msg) {
+        this.snackBar.open(msg, 'Success', {
+            duration: this.autoHide,
+            extraClasses: ['snackbarSuccess']
+        });
+    }
+
+    // 警告
+    public warning(msg) {
+        this.snackBar.open(msg, 'Warning', {
+            duration: this.autoHide,
+            extraClasses: ['snackbarWarning']
+        });
+    }
+    // 失败
+    public error(msg) {
+        this.snackBar.open(msg, 'Error', {
+            duration: this.autoHide,
+            extraClasses: ['snackbarError']
+        });
+    }
+}
