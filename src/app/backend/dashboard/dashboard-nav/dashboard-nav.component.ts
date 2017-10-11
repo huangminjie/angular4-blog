b9767cd6cb525c8062b69360204ed4e4fecabfd1
黄敏杰 <Aghanim@huangminjiedeiMac.local>
2017-10-11 17:16:08 +0800

    添加后台目录

--- src/app/backend/dashboard/dashboard-nav/dashboard-nav.component.ts
+++ src/app/backend/dashboard/dashboard-nav/dashboard-nav.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as menus from '../../../../assets/backend_menu.json';
+
 @Component({
   selector: 'app-dashboard-nav',
   templateUrl: './dashboard-nav.component.html',
   styleUrls: ['./dashboard-nav.component.css']
 })
 export class DashboardNavComponent implements OnInit {
-
+  step = 1;
+  menus = [];
   constructor() { }
 
   ngOnInit() {
+    this.menus = <any>menus;
+  }
+  setStep(index: number) {
+    this.step = index;
   }
 
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
 }
