9ebc6b0aaa11fb5fc0e83ea1e65c5bb69aaca2ff
黄敏杰 <Aghanim@huangminjiedeiMac.local>
2017-10-26 17:52:03 +0800

    引入markdown组件

--- src/app/backend/newpost/newpost.component.ts
+++ src/app/backend/newpost/newpost.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { markdown } from 'markdown'
 
 @Component({
   selector: 'app-backend-newpost',
@@ -6,11 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newpost.component.css']
 })
 export class NewPostComponent implements OnInit {
-
-  constructor() { }
+  postForm: FormGroup;
+  isCollapsed: boolean;
+  options = [];
+  selectedOption;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    console.log(markdown.toHTML("Hello *World*!"));
+    this.options = [
+      { value: 'jack', label: 'Jack' },
+      { value: 'lucy', label: 'Lucy' },
+      { value: 'disabled', label: 'Disabled', disabled: true }
+    ];
+    this.selectedOption = this.options[0];
 
+    this.postForm = this.fb.group({
+      title: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      digest: ['', [Validators.nullValidator]],
+      tag: ['', [Validators.nullValidator]],
+      text: ['', [Validators.required]]
+    });
+    this.postForm.controls['text'].valueChanges.subscribe((data) => {
+      $("#preview").html(markdown.toHTML(data));
+    })
   }
-
+  getFormControl(name) {
+    return this.postForm.controls[name];
+  }
+  collapsed() {
+    if (!this.isCollapsed) {
+      $("#preview").hide();
+    }
+    else {
+      $("#preview").show();
+    }
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
