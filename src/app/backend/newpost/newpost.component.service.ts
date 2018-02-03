9b80c5f0100e1bc7b2df18e14de7b3ab839d4d48
黄敏杰 <Aghanim@huangminjiedeiMac.local>
2018-02-03 18:12:52 +0800

    样式调整

--- src/app/backend/newpost/newpost.component.service.ts
+++ src/app/backend/newpost/newpost.component.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class NewPostService {
 
-    constructor() { }
+    constructor(private http: Http) { }
+
+    getTypes() {
+        return this.http.get('/types').toPromise();
+    }
 }
